fcb780258e80251d2f39306fd1c2d2ceb438d9a9
claudio262603 <claudio25cesar@gmail.com>
2020-09-28 21:26:06 -0500

    Añadir student, professor,subject

---
+++ src/learning/java/school/Professor.java
@@ -0,0 +1,18 @@
+package learning.java.school;
+
+
+
+class Professor {
+    private String name;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+
---
+++ src/learning/java/school/Subject.java
@@ -0,0 +1,23 @@
+package learning.java.school;
+
+public class Subject {
+    private String name;
+    private int credits;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public void setCredits(int credits) {
+        this.credits = credits;
+    }
+}
+
