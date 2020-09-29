2db1ad0982539d59b0cc8eb10e72300fb0cacc0d
claudio262603 <claudio25cesar@gmail.com>
2020-09-28 21:21:27 -0500

    Añadir grupo

---
+++ src/learning/java/school/Group.java
@@ -0,0 +1,18 @@
+package learning.java.school;
+
+
+
+
+
+class Group {
+      private Professor professor;
+    private int availability;
+    private Student[] students;
+
+    Group(int availability) {
+        this.availability = availability;
+        students = new Student[availability];
+    }
+
+}
+
